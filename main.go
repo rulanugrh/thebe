93ec8740a83cc76752ef391ef987575ca6f3e892
rulanugrh <nextarituenak01@gmail.com>
2023-12-08 16:16:15 +0700

    feat: add routes n middleware

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"be-project/config"
+	"be-project/entity/domain"
+	handler "be-project/http"
+	"be-project/repository"
+	"be-project/routes"
+	"be-project/service"
+	"log"
+)
 
+func main() {
+	getDB := config.GetConnection()
+	getDB.AutoMigrate(&domain.Order{}, &domain.Roles{}, &domain.User{})
+
+	orderRepository := repository.NewOrderRepository(getDB)
+	userRepository := repository.NewUserRepository(getDB)
+	roleRepository := repository.NewRoleRepository(getDB)
+
+	userService := service.NewUserService(userRepository)
+	orderService := service.NewOrderService(orderRepository)
+	roleService := service.NewRoleService(roleRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+	orderHandler := handler.NewOrderHandler(orderService)
+	roleHandler := handler.NewRoleHandler(roleService)
+
+	err := routes.Run(userHandler, orderHandler, roleHandler)
+	if err != nil {
+		log.Printf("Cannot running , because: %s", err.Error())
+	}
 }
---
+++ routes/routes.go
@@ -0,0 +1,55 @@
+package routes
+
+import (
+	"be-project/config"
+	portHandler "be-project/http/port"
+	"be-project/middleware"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Run(user portHandler.UserInterface, order portHandler.OrderInterface, role portHandler.RoleInterface) error {
+	router := mux.NewRouter().StrictSlash(true)
+	router.Use(middleware.CommonMiddleware)
+	router.HandleFunc("/register/", user.Register)
+	router.HandleFunc("/login/", user.Login)
+
+	routerGroup := router.PathPrefix("/api/").Subrouter()
+	routerGroup.Use(middleware.CommonMiddleware)
+
+	// routing for user
+	routerGroup.HandleFunc("/user/", user.Update)
+	routerGroup.HandleFunc("/user/{id}", user.Delete)
+
+	// routing for order
+	routerGroup.HandleFunc("/order/", order.Create)
+	routerGroup.HandleFunc("/order/{user_id}", order.FindByUserID)
+	routerGroup.HandleFunc("/order/{id}", order.Update)
+
+	// routing for role
+	routerGroup.HandleFunc("/role/", role.Create)
+	routerGroup.HandleFunc("/role/{id}", role.FindByID)
+	routerGroup.HandleFunc("/role/{id}", role.Update)
+
+	conf := config.GetConfig()
+	host := fmt.Sprintf("%s:%s", conf.App.Host, conf.App.Port)
+
+	server := http.Server{
+		Addr: host,
+		Handler: router,
+	}
+
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Cannot running http server, because: %s", err.Error())
+		return err
+	}
+
+	log.Printf("Server running at: %v", host)
+	return nil
+
+}
