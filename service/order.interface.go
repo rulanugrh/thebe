fee25af91e572aae5d57c7da8298d61338c8d0cc
rulanugrh <nextarituenak01@gmail.com>
2023-12-08 15:09:02 +0700

    feat: order interface impl

---
+++ service/order.interface.go
@@ -0,0 +1,76 @@
+package service
+
+import (
+	"be-project/entity/domain"
+	"be-project/entity/web"
+	portRepo "be-project/repository/port"
+	portService "be-project/service/port"
+	"log"
+)
+
+type orderService struct {
+	repository portRepo.OrderRepository
+}
+
+func NewOrderService(repo portRepo.OrderRepository) portService.OrderInterface {
+	return &orderService{
+		repository: repo,
+	}
+}
+
+func(order *orderService) Create(req domain.Order) (*web.ResponseOrder, error) {
+	data, err := order.repository.Create(req)
+	if err != nil {
+		log.Printf("Cant req order to repo, because: %s", err.Error())
+		return nil, err
+	}
+
+	resultData := web.ResponseOrder{
+		UUID: data.UUID,
+		FName: data.UserDetail.FName,
+		LName: data.UserDetail.LName,
+		Email: data.UserDetail.Email,
+		Address: data.UserDetail.Address,
+		TTL: data.UserDetail.TTL,
+	}
+
+	return &resultData, nil
+}
+
+func(order *orderService) Update(uuid string, req domain.Order) (*web.ResponseOrder, error) {
+	data, err := order.repository.Update(uuid, req)
+	if err != nil {
+		log.Printf("Cant req update to repo, because: %s", err.Error())
+		return nil, err
+	}
+
+	resultData := web.ResponseOrder{
+		UUID: data.UUID,
+		FName: data.UserDetail.FName,
+		LName: data.UserDetail.LName,
+		Email: data.UserDetail.Email,
+		Address: data.UserDetail.Address,
+		TTL: data.UserDetail.TTL,
+	}
+
+	return &resultData, nil
+}
+
+func(order *orderService) FindByUserID(userid uint) (*web.ResponseOrder, error) {
+	data, err := order.repository.FindByUserID(userid)
+	if err != nil {
+		log.Printf("Cant req findbyuser id to repo, because: %s", err.Error())
+		return nil, err
+	}
+
+	resultData := web.ResponseOrder{
+		UUID: data.UUID,
+		FName: data.UserDetail.FName,
+		LName: data.UserDetail.LName,
+		Email: data.UserDetail.Email,
+		Address: data.UserDetail.Address,
+		TTL: data.UserDetail.TTL,
+	}
+
+	return &resultData, nil
+}
