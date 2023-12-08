506ded565ef427a7bbba991ff6a3fcb4f8e82d54
rulanugrh <nextarituenak01@gmail.com>
2023-12-08 13:41:26 +0700

    chore: adding response for order

--- entity/web/responseStruct.go
+++ entity/web/responseStruct.go
@@ -20,5 +20,10 @@ type ResponseRole struct {
 
 type ResponseOrder struct {
 	UUID string `json:"uuid"`
-	User ResponseUser `json:"user"`
+	FName     string `json:"first_name"`
+	LName     string `json:"last_name"`
+	Email     string `json:"email"`
+	Address   string `json:"address"`
+	Telephone string `json:"telephone"`
+	TTL       time.Time `json:"tanggal_lahir"`
 }
