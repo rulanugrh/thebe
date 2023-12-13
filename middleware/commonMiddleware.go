6355dad4ef894dae30fa256c1140a086ff329290
rulanugrh <nextarituenak01@gmail.com>
2023-12-13 09:11:43 -0500

    chore: problems not fix

--- middleware/commonMiddleware.go
+++ middleware/commonMiddleware.go
@@ -4,9 +4,12 @@ import "net/http"
 
 func CommonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, origin, accept")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
 		next.ServeHTTP(w, r)
 	})
 }
