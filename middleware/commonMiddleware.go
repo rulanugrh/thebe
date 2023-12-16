69c0fc94b5268b5f78897c8cd2f98de773bc6366
rulanugrh <nextarituenak01@gmail.com>
2023-12-16 23:41:18 +0700

    chore: credentials

--- middleware/commonMiddleware.go
+++ middleware/commonMiddleware.go
@@ -5,6 +5,7 @@ import "net/http"
 func CommonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
 		w.Header().Add("Content-Type", "application/json")
