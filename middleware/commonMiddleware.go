5f754f47851fee4162c271f8ec1f971d35c48fa4
rulanugrh <nextarituenak01@gmail.com>
2023-12-16 23:42:01 +0700

    chore: credentials

--- middleware/commonMiddleware.go
+++ middleware/commonMiddleware.go
@@ -1,10 +1,14 @@
 package middleware
 
-import "net/http"
+import (
+	"be-project/config"
+	"net/http"
+)
 
 func CommonMiddleware(next http.Handler) http.Handler {
+	conf := config.GetConfig()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Origin", conf.App.AllowOrigin)
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
