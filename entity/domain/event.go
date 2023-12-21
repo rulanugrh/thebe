b19cdde8e7e8f0cb15e3830e0ce0a02efd25a14e
rulanugrh <nextarituenak01@gmail.com>
2023-12-21 12:54:36 +0700

    fix: trouble in relation submission to event

--- entity/domain/event.go
+++ entity/domain/event.go
@@ -4,22 +4,22 @@ import "gorm.io/gorm"
 
 type Event struct {
 	gorm.Model
-	Name         string           `json:"name" form:"name"`
-	Description  string           `json:"desc" form:"desc"`
-	Price        int              `json:"price" form:"price"`
-	Participants []Order          `json:"participants" form:"participants" gorm:"many2many:joined"`
-	Submissions  []Submission     `json:"submissions" form:"submissions" gorm:"many2many:task"`
+	Name         string       `json:"name" form:"name"`
+	Description  string       `json:"desc" form:"desc"`
+	Price        int          `json:"price" form:"price"`
+	Participants []Order      `json:"participants" form:"participants" gorm:"many2many:joined"`
+	Submissions  []Submission `json:"submissions" form:"submissions" gorm:"many2many:task"`
 }
 
 type EventRegister struct {
-	Name         string           `json:"name" form:"name" validate:"required" `
-	Description  string           `json:"desc" form:"desc" validate:"required"`
-	Price        int              `json:"price" form:"price" validate:"required"`
+	Name        string `json:"name" form:"name" validate:"required" `
+	Description string `json:"desc" form:"desc" validate:"required"`
+	Price       int    `json:"price" form:"price" validate:"required"`
 }
 
 type DelegasiParticipant struct {
 	gorm.Model
-	Name        string `json:"name" form:"name"`
+	Name         string `json:"name" form:"name"`
 	Gender       string `json:"gender" form:"gender"`
 	DelegasiID   uint   `json:"delegasi_id" form:"delegasi_id"`
 	DelegasiType string `json:"delegasi_type" form:"delegasi_type"`
--- entity/domain/submission.go
+++ entity/domain/submission.go
@@ -5,18 +5,18 @@ import "gorm.io/gorm"
 type Submission struct {
 	gorm.Model
 	Name    string `json:"name" form:"name"`
-	IDEvent uint   `json:"id_event" form:"id_event"`
-	Events  Event  `json:"events" form:"events" gorm:"foreignKey:IDEvent;references:ID"`
-	IDUser  uint   `json:"id_user" form:"id_user"`
-	Users   User   `json:"users" form:"users" gorm:"foreignKey:IDUser;references:ID"`
-	File   string `json:"file" form:"file"`
-	Video string `json:"link_video" form:"link_video"`
+	EventID uint   `json:"event_id" form:"event_id"`
+	Events  Event  `json:"events" form:"events" gorm:"foreignKey:EventID;references:ID"`
+	UserID  uint   `json:"user_id" form:"user_id"`
+	Users   User   `json:"users" form:"users" gorm:"foreignKey:UserID;references:ID"`
+	File    string `json:"file" form:"file"`
+	Video   string `json:"link_video" form:"link_video"`
 }
 
 type SubmissionTask struct {
 	Name    string `json:"name" form:"name" validate:"required"`
-	IDEvent uint   `json:"id_event" form:"id_event"`
-	IDUser  uint   `json:"id_user" form:"id_user"`
-	Video string `json:"link_video" form:"link_video" validate:"required"`
-	File   string `json:"file" form:"file" validate:"required"`
+	EventID uint   `json:"event_id" form:"event_id"`
+	UserID  uint   `json:"user_id" form:"user_id"`
+	Video   string `json:"link_video" form:"link_video" validate:"required"`
+	File    string `json:"file" form:"file" validate:"required"`
 }
